edbecd44c4130f049ad29a513abf5c6565d6bc6a
afmurillo <andres.murillo.19@gmail.com>
2020-04-07 18:48:22 +0800

    Update into the ctown topology. Now we automatically get the actuators from the INP base file and update the actuators state programmatically if they were changed. This should help with bigger topologies such as ctown. We could start running the simulations

--- ICS_topologies/ctown_topology/physical_process.py
+++ ICS_topologies/ctown_topology/physical_process.py
@@ -3,6 +3,7 @@ import wntr.network.controls as controls
 import sqlite3
 import csv
 import time
+
 import sys
 
 def get_node_list_by_type(list, type):
@@ -12,6 +13,7 @@ def get_node_list_by_type(list, type):
             result.append(str(node))
     return result
 
+
 def get_link_list_by_type(list, type):
     result = []
     for link in list:
@@ -19,11 +21,13 @@ def get_link_list_by_type(list, type):
             result.append(str(link))
     return result
 
+
 def create_node_header(list):
     result = []
     for node in list:
         result.append(node + "_LEVEL")
     return result
+
 
 def create_link_header(list):
     result = []
@@ -33,17 +37,90 @@ def create_link_header(list):
     return result
 
 
-master_time = 0
+def get_controls(list):
+    result = []
+    for control in list:
+        result.append(wn.get_control(control))
+    return result
+
+
+def create_control_dict(actuator):
+    act_dict = dict.fromkeys(['actuator', 'parameter', 'value', 'condition', 'name'])
+    act_dict['actuator'] = wn.get_link(actuator)
+    act_dict['parameter'] = 'status'
+    act_dict['condition'] = dummy_condition
+    act_dict['name'] = actuator + "_control"
+    if type(wn.get_link(actuator).status) is int:
+        act_dict['value'] = act_dict['actuator'].status
+    else:
+        act_dict['value'] = act_dict['actuator'].status.value
+    return act_dict
+
+
+def register_results(results):
+    values_list = []
+    values_list.extend([results.timestamp])
+
+    # Results are divided into: nodes: reservoir and tanks, links: flows and status
+    # Get tanks levels
+    for tank in tank_list:
+        values_list.extend([wn.get_node(tank).level])
+
+    # Get junction  levels
+    for junction in junction_list:
+        values_list.extend([wn.get_node(junction).head - wn.get_node(junction).elevation])
+
+    # Get pumps flows and status
+    for pump in pump_list:
+        values_list.extend([wn.get_link(pump).flow])
+
+        if type(wn.get_link(pump).status) is int:
+            values_list.extend([wn.get_link(pump).status])
+        else:
+            values_list.extend([wn.get_link(pump).status.value])
+
+            # Get valves flows and status
+    for valve in valve_list:
+        values_list.extend([wn.get_link(valve).flow])
+
+        if type(wn.get_link(valve).status) is int:
+            values_list.extend([wn.get_link(valve).status])
+        else:
+            values_list.extend([wn.get_link(valve).status.value])
+    return values_list
+
+def update_actuator_values():
+    for actuator in control_list:
+        update_actuator(actuator)
+
+def update_actuator(actuator):
+    rows_1 = c.execute("SELECT value FROM ctown WHERE name = actuator['name']").fetchall()
+    conn.commit()
+
+    new_status = rows_1[0][0]
+    if new_status != actuator['value']:
+        actuator['value'] = new_status
+
+        #act1 = controls.ControlAction(pump1, 'status', int(pump1_status))
+        new_action  = controls.ControlAction(control['actuator'], control['parameter'], control['value'])
+
+        #pump1_control = controls.Control(condition, act1, name='pump1control')
+        new_control = controls.Control(control['condition'], new_action, control['name'])
+
+        wn.remove_control(actuator['name'])
+        wn.add_control(actuator['name'], new_control)
 
 # connection to the database
-conn = sqlite3.connect('ctown_db.sqlite')
+conn = sqlite3.connect('minitown_db.sqlite')
 c = conn.cursor()
+
+dummy_condition = controls.ValueCondition(wn.get_node('T1'), 'level', '>=', -1)
 
 # Create the network
 inp_file = sys.argv[2]+'_map.inp'
 wn = wntr.network.WaterNetworkModel(inp_file)
 
-# All times in seconds
+# We define the simulation times in seconds
 wn.options.time.duration = 1
 wn.options.time.hydraulic_timestep = 5*60
 wn.options.time.quality_timestep = 5*60
@@ -54,11 +131,12 @@ list_header = []
 node_list = list(wn.node_name_list)
 link_list = list(wn.link_name_list)
 
-tank_list = get_node_list_by_type(node_list, 'Tank')
-junction_list = get_node_list_by_type(node_list, 'Junction')
-pump_list = get_link_list_by_type(link_list, 'Pump')
-valve_list = get_link_list_by_type(link_list, 'Valve')
-
+#reservoir_list = get_node_list_by_type(node_list, 'Reservoir')
+tank_list      = get_node_list_by_type(node_list, 'Tank')
+junction_list  = get_node_list_by_type(node_list, 'Junction')
+pump_list      = get_link_list_by_type(link_list, 'Pump')
+valve_list     = get_link_list_by_type(link_list, 'Valve')
+#control_names  = wn.control_name_list
 
 list_header = ["Timestamps"]
 aux = create_node_header(tank_list)
@@ -73,14 +151,24 @@ list_header.extend(aux)
 aux = create_link_header(valve_list)
 list_header.extend(aux)
 
-results_list = [list_header]
+results_list = []
+results_list.append(list_header)
 
-# We define a dummy condition that should always be true
-condition = controls.ValueCondition(wn.get_node('T1'), 'level', '>=', -1)
+#control_list_str = get_controls(control_names)
 
-# Create an action and control object for each actuator. Add that control object to the wn
+control_list = []
+for valve in valve_list:
+    control_list.append(create_control_dict(valve))
+
+for pump in pump_list:
+    control_list.append(create_control_dict(pump))
 
 
+for control in control_list:
+    #act1 = controls.ControlAction(pump1, 'status', int(pump1_status))
+    an_action = controls.ControlAction(control['actuator'], control['parameter'], control['value'])
+    a_control = controls.Control(control['condition'], an_action, control['name'])
+    wn.add_control(control['name'], a_control)
 
 if sys.argv[1] == 'pdd':
     print('Running simulation using PDD')
@@ -92,67 +180,16 @@ else:
     print('Invalid simulation mode, exiting...')
     sys.exit(1)
 
-days_simulated = 1
-iteration = 0
-iteration_limit = days_simulated*(24*3600) / wn.options.time.duration
+master_time = 0
+days = 1
+iteration_limit = (days*24*3600)/wn.options.time.hydraulic_timestep
 
-# START STEP BY STEP SIMULATION
-while iteration <= iteration_limit:
+while master_time <= iteration_limit:
 
-    # Get updated values from the database of : -VALVE VALUE -PUMPS VALUES
-    #  UPDATE WNTR PUMP1 STATUS FROM DATABASE
-    rows_1 = c.execute("SELECT value FROM minitown WHERE name = 'P1_STS'").fetchall()
-    conn.commit()
-
-    rows_2 = c.execute("SELECT value FROM minitown WHERE name = 'P2_STS'").fetchall()
-    conn.commit()
-
-    pump1_status = rows_1[0][0]  # PUMP1 STATUS FROM DATABASE
-    act1 = controls.ControlAction(pump1, 'status', int(pump1_status))
-    pump1_control = controls.Control(condition, act1, name='pump1control')
-
-    #  UPDATE WNTR PUMP2 STATUS FROM DATABASE
-    pump2_status = rows_2[0][0]  # PUMP1 STATUS FROM DATABASE
-    act2 = controls.ControlAction(pump2, 'status', int(pump2_status))
-    pump2_control = controls.Control(condition, act2, name='pump2control')
-
-    wn.remove_control("WnPump1Control")
-    wn.remove_control("WnPump2Control")
-
-    wn.add_control('WnPump1Control', pump1_control)
-    wn.add_control('WnPump2Control', pump2_control)
+    #update_actuator_values()
 
     results = sim.run_sim(convergence_error=True)
-
-    print ("ITERATION %d ------------------" % iteration)
-
-    c.execute("UPDATE minitown SET value = %f WHERE name = 'T_LVL'" % tank.level)  # UPDATE TANK LEVEL IN THE DATABASE
-    conn.commit()
-
-    # take the value of attacks labels from the database
-    rows = c.execute("SELECT value FROM minitown WHERE name = 'ATT_1'").fetchall()
-    conn.commit()
-    attack1 = rows[0][0]
-
-    rows = c.execute("SELECT value FROM minitown WHERE name = 'ATT_2'").fetchall()
-    conn.commit()
-    attack2 = rows[0][0]
-
-    values_list = []
-
-    values_list.extend([results.timestamp, tank.level, reservoir.head])
-    for junction in junction_list:
-        values_list.extend([wn.get_node(junction).head - wn.get_node(junction).elevation])
-
-    values_list.extend([pump1.flow, pump2.flow, pump1_status, pump2_status, attack1, attack2])
+    values_list = register_results(results)
 
     results_list.append(values_list)
-
-    iteration += 1
-
-    if results:
-        time.sleep(0.5)
-
-with open('output/'+sys.argv[3], 'w', newline='\n') as f:
-    writer = csv.writer(f)
-    writer.writerows(results_list)
+    master_time += 1
