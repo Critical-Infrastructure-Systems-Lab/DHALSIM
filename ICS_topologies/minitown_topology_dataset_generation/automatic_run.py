5ee66b0aea95e0e43ab8573a3bf2dd6f2b2f9a52
afmurillo <andres.murillo.19@gmail.com>
2020-04-16 00:39:09 +0800

    Working on the sync problem in Minitown. Now the "no mutex" topology also has the same initial conditions as WNTR, the difference in the behavior of the pump flows is now the same
    Former-commit-id: ea75f21ed58fc74118124629650fdd1a91aecc45

--- ICS_topologies/minitown_topology_dataset_generation/automatic_run.py
+++ ICS_topologies/minitown_topology_dataset_generation/automatic_run.py
@@ -6,7 +6,7 @@ import sys
 import time
 import subprocess
 
-automatic = 1
+automatic = 0
 
 class Minitown(MiniCPS):
     """ Script to run the Minitown SCADA topology """
--- ICS_topologies/minitown_topology_dataset_generation/plc1.py
+++ ICS_topologies/minitown_topology_dataset_generation/plc1.py
@@ -1,14 +1,16 @@
 from minicps.devices import PLC
 from utils import PLC1_DATA, STATE, PLC1_PROTOCOL
-from utils import T_LVL, ATT_1, PLC1_ADDR, flag_attack_plc1, flag_attack_plc2, flag_attack_communication_plc1_scada, \
-    flag_attack_communication_plc1_plc2, flag_attack_dos_plc2, CONTROL
+from utils import T_LVL, ATT_1, PLC1_ADDR, flag_attack_plc1, flag_attack_plc2, \
+    flag_attack_communication_plc1_scada, flag_attack_communication_plc1_plc2, flag_attack_dos_plc2, CONTROL
 
 import csv
-import time
-
 from datetime import datetime
 from decimal import Decimal
+
+import time
 import thread
+
+plc1_log_path = 'plc1.log'
 tank_level=3.0
 
 def write_output(saved_tank_levels):
@@ -22,6 +24,11 @@ class PLC1(PLC):
 
     def pre_loop(self):
         print 'DEBUG: plc1 enters pre_loop'
+        # threading
+        self.local_time = 0
+
+        #non-threading
+        self.tank_level = Decimal(self.get(T_LVL))
 
     def main_loop(self):
         """plc1 main loop.
@@ -29,13 +36,39 @@ class PLC1(PLC):
             - drives actuators according to the control strategy
             - updates its enip server
         """
-        saved_tank_levels = ["timestamp", "TANK_LEVEL"]
+        fake_values = []
+        saved_tank_levels = [["iteration", "timestamp", "TANK_LEVEL"]]
 
         while True:
             try:
+                self.local_time += 1
+
+                #threading
+                #global tank_level
+                #saved_tank_levels.append([datetime.now(), tank_level])
+                #self.send(T_LVL, tank_level, PLC1_ADDR)
+                #print("Tank Level %f " % tank_level)
+
+                #non threading
                 self.tank_level = Decimal(self.get(T_LVL))
-                saved_tank_levels.append([datetime.now(), tank_level])
+                saved_tank_levels.append([datetime.now(), self.tank_level])
                 self.send(T_LVL, self.tank_level, PLC1_ADDR)
+                print("Tank Level %f " % self.tank_level)
+
+                if flag_attack_plc1:
+                    if self.local_time in range(100, 200):
+                        print("Attacker is appending --------------")
+                        fake_values.append(tank_level)
+                        self.set(ATT_1, 1)
+                    elif self.local_time in range(250, 350):
+                        print("Under Attack---------------------- ")
+                        self.set(ATT_1, 2)
+                        tank_level = fake_values[self.local_time]
+                        self.local_time += 1
+                    else:
+                        if flag_attack_plc2 == 0 and flag_attack_communication_plc1_scada == 0 and flag_attack_communication_plc1_plc2 == 0 and flag_attack_dos_plc2 == 0:
+                            self.set(ATT_1, 0)
+
             except KeyboardInterrupt:
                 write_output(saved_tank_levels)
                 return
