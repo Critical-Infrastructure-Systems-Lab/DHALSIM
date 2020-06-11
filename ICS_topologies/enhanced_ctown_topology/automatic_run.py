d85783243c4ef13a8708d1c0fa490715794b3425
afmurillo <andres.murillo.19@gmail.com>
2020-06-11 16:38:43 +0800

    Implemented the proof concept of the enhanced ctown topology using NAT and port forwarding with iptables. The PLCs can now talk each other using port forwarding. The issue now is configuring MiniCPS to bind the ENIP server to a specific port
    Former-commit-id: 1f21fd49fe0d750d0d8116e8ca2030e607a03462

--- ICS_topologies/enhanced_ctown_topology/automatic_run.py
+++ ICS_topologies/enhanced_ctown_topology/automatic_run.py
@@ -11,26 +11,37 @@ import signal
 class CTown(MiniCPS):
     """ Script to run the Minitown SCADA topology """
 
+    def setup_iptables(self, node_name):
+        a_node = net.get(node_name)
+        a_node.cmd('bash ./ctown_nat.sh ' + node_name)
+        a_node.waitOutput()
+
+        a_node.cmd('bash ./port_forward.sh ' + node_name)
+        a_node.waitOutput()
+
     def do_forward(self, node):
         # Pre experiment configuration, prepare routing path
         node.cmd('sysctl net.ipv4.ip_forward=1')
         node.waitOutput()
 
-    def add_degault_gateway(self, node):
-        node.cmd('roude add default gw 192.168.1.254')
+    def add_degault_gateway(self, node, gw_ip):
+        node.cmd('route add default gw ' + gw_ip)
         node.waitOutput()
 
     def __init__(self, name, net):
         net.start()
 
-        r0 = net.get('r0')
-        self.do_forward(r0)
-
         for i in range(0, 3):
-            self.do_forward( net.get('r' + str(i) ) )
+            self.do_forward(net.get('r' + str(i)))
 
-        self.add_degault_gateway( net.get('plc1') )
-        self.add_degault_gateway( net.get('plc2') )
+        self.add_degault_gateway(net.get('plc1'), '192.168.1.254')
+        self.add_degault_gateway(net.get('plc2'), '192.168.1.254')
+
+        self.add_degault_gateway(net.get('r1'), '10.0.1.254')
+        self.add_degault_gateway(net.get('r2'), '10.0.2.254')
+
+        for i in range(1, 3):
+            self.setup_iptables('r' + str(i))
 
         CLI(net)
         net.stop()
--- ICS_topologies/enhanced_ctown_topology/topo.py
+++ ICS_topologies/enhanced_ctown_topology/topo.py
@@ -29,28 +29,20 @@ class CTownTopo(Topo):
         switches = []
         plcs = []
 
-        r0_ip = '10.0.1.254/16'
+        r0_ip = '10.0.1.254/24'
         net_prefix = '10.0.'
 
         #  ---------------- Central Router (r0) ----------------------  #
         r0 = self.addNode('r0', cls=LinuxRouter, ip=r0_ip)
-        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.1.1')
-        r2 = self.addNode('r2', cls=LinuxRouter, ip='10.0.2.1')
+        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.1.1/24')
+        r2 = self.addNode('r2', cls=LinuxRouter, ip='10.0.2.1/24')
 
-        self.addLink(r1, r0, intfName2='r0-eth0', params2={'ip': '10.0.1.254/16'})
-        self.addLink(r2, r0, intfName2='r0-eth1', params2={'ip': '10.0.2.254/16'})
+        self.addLink(r1, r0, intfName2='r0-eth0', params2={'ip': '10.0.1.254/24'})
+        self.addLink(r2, r0, intfName2='r0-eth1', params2={'ip': '10.0.2.254/24'})
 
         plc1 = self.addNode('plc1', ip='192.168.1.1/24', defaultRoute='via 192.168.1.254/24')
         self.addLink(plc1, r1, intfName2='r1-eth1', params2={'ip': '192.168.1.254/24'})
 
         plc2 = self.addNode('plc2', ip='192.168.1.1/24', defaultRoute='via 192.168.1.254/24')
         self.addLink(plc2, r2, intfName2='r2-eth1', params2={'ip': '192.168.1.254/24'})
-
-        """"
-        for i in range(1, 10):
-            routers.append(self.addNode('r' + str(i), cls=LinuxRouter, ip=net_prefix + str(i) + '.1/16'))
-            self.addLink(routers[i - 1], r0, intfName2='r0-eth' + str(i),
-                         params2={'ip': net_prefix + str(i) + '.254/16'})
-        """
-
 
