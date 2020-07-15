b0999cdd39d34421a384d2a0796c3fe38b25f766
afmurillo <andres.murillo.19@gmail.com>
2020-07-16 00:31:04 +0800

    Added executable flags to scripts

--- ICS_topologies/enhanced_ctown_topology/port_forward.sh
+++ ICS_topologies/enhanced_ctown_topology/port_forward.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -A PREROUTING -t nat -i $1-eth0 -p tcp --dport 80 -j DNAT --to 192.168.1.1:8080
-iptables -A FORWARD -p udp -d 192.168.1.1 --dport 8080 -j ACCEPT
+iptables -A PREROUTING -t nat -i $1-eth0 -p tcp --dport 44818 -j DNAT --to 192.168.1.1:44818
+iptables -A FORWARD -p tcp -d 192.168.1.1 --dport 44818 -j ACCEPT
--- ICS_topologies/enhanced_ctown_topology/show_ip_tables.sh
+++ ICS_topologies/enhanced_ctown_topology/show_ip_tables.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 iptables -t nat -v -L PREROUTING
 iptables -L FORWARD
 iptables -t nat -v -L POSTROUTING
