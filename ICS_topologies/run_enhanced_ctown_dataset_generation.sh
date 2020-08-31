44032f5ab14913a4b1397d01746ffa5561d42eb4
Andres <andres.murillo.19@gmail.com>
2020-08-31 12:26:20 +0800

    adding the script controlling the year c-town simulation
    Former-commit-id: 358c9b2d0fe10caed2cea3cbc3e744895d8b950c

---
+++ ICS_topologies/run_enhanced_ctown_dataset_generation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {0..12}
+do
+	if [ ! -d enhanced_ctown_topology/logs ]; then\
+	   mkdir logs;\
+	fi
+	if [ ! -d enhanced_ctown_topology/output ]; then\
+	   mkdir output;\
+	fi
+	cd enhanced_ctown_topology; rm -rf ctown_db.sqlite; python init.py; sudo chown mininet:mininet ctown_db.sqlite
+	sudo pkill  -f -u root "python -m cpppo.server.enip"
+	sudo mn -c
+	sudo python automatic_run.py $i
+
+	sudo pkill  -f -u root "python -m cpppo.server.enip"
+	sudo mn -c
+done
