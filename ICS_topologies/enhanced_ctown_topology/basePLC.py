15ba96addcede41f4542759ea652c8231d1e41e9
Andres <andres.murillo.19@gmail.com>
2020-10-13 23:36:50 +0800

    Updating the PLCs in enchanced_ctown to use the BasePLC class

---
+++ ICS_topologies/enhanced_ctown_topology/basePLC.py
@@ -0,0 +1,25 @@
+from minicps.devices import PLC
+import csv
+import signal
+import sys
+
+
+class BasePLC(PLC):
+
+    def set_parameters(self, path, result_list):
+        self.result_list = result_list
+        self.path = path
+
+    def write_output(self):
+        with open('output/' + self.path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.result_list)
+
+    def sigint_handler(self, sig, frame):
+        print 'DEBUG plc shutdown'
+        self.write_output()
+        sys.exit(0)
+
+    def startup(self):
+        signal.signal(signal.SIGINT, self.sigint_handler)
+        signal.signal(signal.SIGTERM, self.sigint_handler)
