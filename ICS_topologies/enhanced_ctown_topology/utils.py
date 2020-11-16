0c7c696804ce50e346b180d7b59db3a9967f45cd
afmurillo <andres.murillo.19@gmail.com>
2020-11-16 17:21:20 +0800

    Finished the first stage of the epanet parser.The parser can now use the information in an epanet INP and epanetCPA file to produce a utils.py file and a dictionary to create a generic PLC code.

--- ICS_topologies/enhanced_ctown_topology/utils.py
+++ ICS_topologies/enhanced_ctown_topology/utils.py
@@ -1,22 +1,25 @@
-T5 = ('T5', 1)
-T7 = ('T7', 1)
-T6 = ('T6', 1)
-T3 = ('T3', 1)
-T2 = ('T2', 1)
-T4 = ('T4', 1)
-T1 = ('T1', 1)
+v1 = ('v1', 1)
+V45 = ('V45', 1)
+V47 = ('V47', 1)
+V2 = ('V2', 1)
+PU1 = ('PU1', 1)
+PU2 = ('PU2', 1)
+PU3 = ('PU3', 1)
 PU4 = ('PU4', 1)
 PU5 = ('PU5', 1)
-V2 = ('V2', 1)
 PU6 = ('PU6', 1)
-PU11 = ('PU11', 1)
-PU8 = ('PU8', 1)
-PU10 = ('PU10', 1)
 PU7 = ('PU7', 1)
+PU8 = ('PU8', 1)
 PU9 = ('PU9', 1)
-PU1 = ('PU1', 1)
-PU3 = ('PU3', 1)
-PU2 = ('PU2', 1)
+PU10 = ('PU10', 1)
+PU11 = ('PU11', 1)
+T1 = ('T1', 1)
+T2 = ('T2', 1)
+T3 = ('T3', 1)
+T4 = ('T4', 1)
+T5 = ('T5', 1)
+T6 = ('T6', 1)
+T7 = ('T7', 1)
 
 plc_netmask = '/24'
 ENIP_LISTEN_PLC_ADDR = '192.168.1.1'
@@ -78,11 +81,11 @@ PLC1_TAGS = (
     ('PU1', 1, 'REAL'),
     ('PU2', 1, 'REAL'),
     ('PU3', 1, 'REAL'),
-    ('T1', 1, 'REAL')
+    ('T1', 1, 'REAL'),
 )
 
 PLC2_TAGS = (
-    ('T1', 1, 'REAL')
+    ('T1', 1, 'REAL'),
 )
 
 PLC3_TAGS = (
@@ -92,13 +95,13 @@ PLC3_TAGS = (
     ('PU6', 1, 'REAL'),
     ('PU7', 1, 'REAL'),
     ('V2', 1, 'REAL'),
-    ('T2', 1, 'REAL'),
     ('T3', 1, 'REAL'),
-    ('T4', 1, 'REAL')
+    ('T4', 1, 'REAL'),
+    ('T2', 1, 'REAL'),
 )
 
 PLC4_TAGS = (
-    ('T3', 1, 'REAL')
+    ('T3', 1, 'REAL'),
 )
 
 PLC5_TAGS = (
@@ -107,45 +110,48 @@ PLC5_TAGS = (
     ('PU10', 1, 'REAL'),
     ('PU11', 1, 'REAL'),
     ('T7', 1, 'REAL'),
-    ('T5', 1, 'REAL')
+    ('T5', 1, 'REAL'),
 )
 
 PLC6_TAGS = (
-    ('T4', 1, 'REAL')
+    ('T4', 1, 'REAL'),
 )
 
 PLC7_TAGS = (
-    ('T5', 1, 'REAL')
+    ('T5', 1, 'REAL'),
 )
 
 PLC8_TAGS = (
-    ('T6', 1, 'REAL')
+    ('T6', 1, 'REAL'),
 )
 
 PLC9_TAGS = (
-    ('T7', 1, 'REAL')
+    ('T7', 1, 'REAL'),
 )
 
 SCADA_TAGS = (
-    ('T5', 1, 'REAL'),
-    ('T7', 1, 'REAL'),
-    ('T6', 1, 'REAL'),
-    ('T3', 1, 'REAL'),
-    ('T2', 1, 'REAL'),
-    ('T4', 1, 'REAL'),
-    ('T1', 1, 'REAL'),
+    ('v1', 1, 'REAL'),
+    ('V45', 1, 'REAL'),
+    ('V47', 1, 'REAL'),
+    ('V2', 1, 'REAL'),
+    ('PU1', 1, 'REAL'),
+    ('PU2', 1, 'REAL'),
+    ('PU3', 1, 'REAL'),
     ('PU4', 1, 'REAL'),
     ('PU5', 1, 'REAL'),
-    ('V2', 1, 'REAL'),
     ('PU6', 1, 'REAL'),
-    ('PU11', 1, 'REAL'),
-    ('PU8', 1, 'REAL'),
-    ('PU10', 1, 'REAL'),
     ('PU7', 1, 'REAL'),
+    ('PU8', 1, 'REAL'),
     ('PU9', 1, 'REAL'),
-    ('PU1', 1, 'REAL'),
-    ('PU3', 1, 'REAL'),
-    ('PU2', 1, 'REAL')
+    ('PU10', 1, 'REAL'),
+    ('PU11', 1, 'REAL'),
+    ('T1', 1, 'REAL'),
+    ('T2', 1, 'REAL'),
+    ('T3', 1, 'REAL'),
+    ('T4', 1, 'REAL'),
+    ('T5', 1, 'REAL'),
+    ('T6', 1, 'REAL'),
+    ('T7', 1, 'REAL'),
 )
 
 flag_attack_communication_plc1_plc2_replay_empty = 0
@@ -266,23 +272,28 @@ CREATE TABLE ctown (
 """
 
 SCHEMA_INIT = """
-    INSERT INTO ctown VALUES ('T5', 1, '1.0');
-    INSERT INTO ctown VALUES ('T7', 1, '2.5');
-    INSERT INTO ctown VALUES ('T6', 1, '5.2');
-    INSERT INTO ctown VALUES ('T3', 1, '3.0');
-    INSERT INTO ctown VALUES ('T2', 1, '0.5');
-    INSERT INTO ctown VALUES ('T4', 1, '2.5');
     INSERT INTO ctown VALUES ('T1', 1, '3.0');
+    INSERT INTO ctown VALUES ('T2', 1, '0.5');
+    INSERT INTO ctown VALUES ('T3', 1, '3.0');
+    INSERT INTO ctown VALUES ('T4', 1, '2.5');
+    INSERT INTO ctown VALUES ('T5', 1, '1.0');
+    INSERT INTO ctown VALUES ('T6', 1, '5.2');
+    INSERT INTO ctown VALUES ('T7', 1, '2.5');
+    INSERT INTO ctown VALUES ('v1', 1, '2');
+    INSERT INTO ctown VALUES ('V45', 1, '2');
+    INSERT INTO ctown VALUES ('V47', 1, '2');
+    INSERT INTO ctown VALUES ('V2', 1, '0');
+    INSERT INTO ctown VALUES ('PU1', 1, '0');
+    INSERT INTO ctown VALUES ('PU2', 1, '1');
+    INSERT INTO ctown VALUES ('PU3', 1, '0');
     INSERT INTO ctown VALUES ('PU4', 1, '0');
     INSERT INTO ctown VALUES ('PU5', 1, '0');
-    INSERT INTO ctown VALUES ('V2', 1, '0');
     INSERT INTO ctown VALUES ('PU6', 1, '0');
-    INSERT INTO ctown VALUES ('PU11', 1, '0');
-    INSERT INTO ctown VALUES ('PU8', 1, '0');
-    INSERT INTO ctown VALUES ('PU10', 1, '0');
     INSERT INTO ctown VALUES ('PU7', 1, '0');
+    INSERT INTO ctown VALUES ('PU8', 1, '0');
     INSERT INTO ctown VALUES ('PU9', 1, '0');
-    INSERT INTO ctown VALUES ('PU1', 1, '0');
-    INSERT INTO ctown VALUES ('PU3', 1, '0');
-    INSERT INTO ctown VALUES ('PU2', 1, '1');
+    INSERT INTO ctown VALUES ('PU10', 1, '0');
+    INSERT INTO ctown VALUES ('PU11', 1, '0');
+    INSERT INTO ctown VALUES ('ATT_1', 1, '0');
+    INSERT INTO ctown VALUES ('ATT_2', 1, '0');
 """
