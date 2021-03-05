903d357c210a2dc60febde39f2eb76e6b842ad68
Andres <andres.murillo.19@gmail.com>
2021-03-05 18:19:39 +0800

    Adding a file that had caused conflict
    Former-commit-id: 9ef3f652d94c52e9b563cbd7a574306f69acde40

--- ICS_topologies/run_minitown_dataset_generation.sh
+++ ICS_topologies/run_minitown_dataset_generation.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for i in {0..12}
+for i in {27..30}
 do
 	echo "Running week " $i
 
-	if [ ! -d minitown_topology_dataset_generation/logs ]; then\
-	   mkdir minitown_topology_dataset_generation/logs;\
+	if [ ! -d minitown_topology/logs ]; then\
+	   mkdir minitown_topology/logs;\
 	fi
 
-	if [ ! -d minitown_topology_dataset_generation/output ]; then\
-	   mkdir minitown_topology_dataset_generation/output;\
+	if [ ! -d minitown_topology/output ]; then\
+	   mkdir minitown_topology/output;\
 	fi
 
-	cd minitown_topology_dataset_generation; rm -rf minitown_db.sqlite; python init.py; sudo chown mininet:mininet minitown_db.sqlite
+	cd minitown_topology; rm -rf minitown_db.sqlite; python init.py; sudo chown mininet:mininet minitown_db.sqlite
 	sudo pkill  -f -u root "python -m cpppo.server.enip"
 	sudo mn -c
 	sudo python automatic_run.py $i
