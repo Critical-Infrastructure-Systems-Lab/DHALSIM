9d47aa89c95894b2bd8f3d2919e4bd7bc7be4e3e
Maarten Weyns <maarten.weyns@mawey.be>
2021-04-29 19:23:17 +0200

    Added a test test for the runner

---
+++ test/Basic_Test.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class Testing(unittest.TestCase):
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
