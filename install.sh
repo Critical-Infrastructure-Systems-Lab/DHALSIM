f8dcab14dffb41a19da8008aa92ff03823eafe9c
Robert van Dijk <r.vandijk-10@student.tudelft.nl>
2021-04-29 14:36:49 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+cd ~
+
 git clone git://github.com/mininet/mininet.git
 
-PYTHON=python3 util/install.sh -fnv
+PYTHON=python3 ~/mininet/util/install.sh -fnv
 
 git clone git@gitlab.ewi.tudelft.nl:cse2000-software-project/2020-2021-q4/cluster-06/water-infrastructure/minicps.git
 
+cd minicps
+
 sudo python3 -m pip install -r ~/minicps/requirements-dev.txt
+
+sudo python3 -m pip install .
+
+cd ~
