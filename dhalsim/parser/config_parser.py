d652a7143236e12d007d3bb82505ab4d4c9289a7
Robert van Dijk <r.vandijk-10@student.tudelft.nl>
2021-04-30 13:51:34 +0200

    Add getter for inp file

--- dhalsim/parser/config_parser.py
+++ dhalsim/parser/config_parser.py
@@ -9,18 +9,12 @@ logger = logging.getLogger(__name__)
 class ConfigParser():
     def __init__(self, config_path):
 
-        config_path = os.path.abspath(config_path)
+        self.config_path = os.path.abspath(config_path)
 
         logger.debug("config file: %s", config_path)
 
         with open(config_path) as f:
             self.config_data = yaml.load(f, Loader=yaml.FullLoader)
-
-        self.inp_path = self.config_data.get("inp_file")
-        self.inp_path = os.path.join(os.path.dirname(config_path), self.inp_path)
-        self.inp_path = os.path.abspath(self.inp_path)
-
-        logger.debug("inp file: %s", self.inp_path)
 
         cpa_path = self.config_data.get("cpa_file")
         cpa_path = os.path.join(os.path.dirname(config_path), cpa_path)
@@ -31,6 +25,14 @@ class ConfigParser():
         with open(cpa_path) as f:
             self.cpa_data = yaml.load(f, Loader=yaml.FullLoader)
 
+    @property
+    def inp_path(self):
+        p = self.config_data.get("inp_file")
+        p = os.path.join(os.path.dirname(self.config_path), p)
+        p = os.path.abspath(p)
+        return p
+
 if __name__ == '__main__':
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     parser = ConfigParser(sys.argv[1])
+    print(parser.inp_path)
