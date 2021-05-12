6f9d90892e36782dc135125bfb9ebcf0b06dd21c
Luc Jonker <l.jonker@student.tudelft.nl>
2021-05-12 15:34:31 +0200

    Bugfix generic_plc.py
    With this commit, the generic PLC is running properly, with no control actions
    
    Co-authored-by: Robert van Dijk <r.vandijk-10@student.tudelft.nl>

--- dhalsim/python2/automatic_plc.py
+++ dhalsim/python2/automatic_plc.py
@@ -1,9 +1,12 @@
+import os
 import subprocess
 import time
 import sys
 import argparse
 import signal
 import shlex
+import yaml
+from pathlib import Path
 
 
 class NodeControl:
@@ -25,9 +28,6 @@ class NodeControl:
         sys.exit(0)
 
     def terminate(self):
-        """
-        All the subprocesses launched in this Digital Twin follow the same pattern to ensure that they finish before continuing with the finishing of the parent process
-        """
         print "Stopping Tcp dump process on PLC..."
         # self.process_tcp_dump.kill()
 
@@ -58,65 +58,8 @@ class NodeControl:
 
         self.terminate()
 
-    def process_arguments(self, arg_parser):
-        if arg_parser.name:
-            self.name = arg_parser.name
-            print self.name
-        else:
-            self.name = 'plc1'
-
-        if arg_parser.week:
-            self.week_index = arg_parser.week
-        else:
-            self.week_index = 0
-
-        if arg_parser.index:
-            self.plc_index = arg_parser.index
-        else:
-            print "NO INDEX PROVIDED"
-
-        if arg_parser.dict:
-            self.dict_path = arg_parser.dict
-        else:
-            self.dict_path = 'plc_dicts.yaml'
-
-        if arg_parser.yaml:
-            self.yaml_path = arg_parser.yaml
-        else:
-            raise IOError
-
-        if arg_parser.attack_flag:
-            self.attack_flag = True
-            if arg_parser.attack_path:
-                self.attack_path = arg_parser.attack_path
-            else:
-                self.attack_path = "../../attack_repository/wadi_attack_description.yaml"
-
-            if arg_parser.attack_name:
-                self.attack_name = arg_parser.attack_name
-            else:
-                self.attack_name = "plc_empty_tank_1"
-        else:
-            self.attack_flag = False
-            self.attack_path = None
-            self.attack_name = None
-
     def delete_log(self):
-        """
-        We delete the log of previous experiments
-        :return:
-        """
-        subprocess.call(['rm', '-rf', self.name + '.log'])
-
-    def configure_routing(self):
-        self.interface_name = self.name + '-eth0'
-        if self.name == 'scada':
-            routing = subprocess.call(['route', 'add', 'default', 'gw', '192.168.2.254', self.interface_name],
-                                      shell=False)
-        else:
-            routing = subprocess.call(['route', 'add', 'default', 'gw', '192.168.1.254', self.interface_name],
-                                      shell=False)
-        return routing
+        subprocess.call(['rm', '-rf', self.intermediate_plc["name"] + '.log'])
 
     def start_tcpdump_capture(self):
         pcap = self.interface_name + '.pcap'
@@ -124,32 +67,26 @@ class NodeControl:
         return tcp_dump
 
     def start_plc(self):
-        cmd_string = 'python2 generic_plc.py' + ' -i ' + str(self.plc_index) + ' -w ' \
-                     + str(self.week_index) + ' -y ' + str(self.yaml_path)
+        cmd = ["python2", "generic_plc.py", "-i", str(self.plc_index), "-y", str(self.intermediate_yaml)]
 
-        # if self.attack_flag:
-        #     # Pass the path to the PLC so it can parse the attack information and run it
-        #     cmd_string = 'python ' + self.name + '.py' + ' -w ' + str(self.week_index) + ' -f True -p ' + \
-        #                  str(self.attack_path) + ' -a ' + str(self.attack_name)
-
-        cmd = shlex.split(cmd_string)
         plc_process = subprocess.Popen(cmd, shell=False, stderr=sys.stderr, stdout=sys.stdout)
         return plc_process
 
-    def get_arguments(self):
-        parser = argparse.ArgumentParser(description='Master Script of a node in Minicps')
-        parser.add_argument("--name", "-n", help="Name of the Mininet node and script to run")
-        parser.add_argument("--week", "-w", help="Week index of the simulation")
-        parser.add_argument("--yaml", "-y", help="Path to intermediate yaml")
-        parser.add_argument("--index", "-i", help="Index of PLC in intermediate yaml")
-        parser.add_argument("--dict", "-d", help="Dictionary of the PLCs logic")
-        parser.add_argument("--attack_flag", "-f", help="Flag to indicate if this PLC needs to run an attack")
-        parser.add_argument("--attack_path", "-p", help="Path to the attack repository")
-        parser.add_argument("--attack_name", "-a", help="Name of the attack to be run by this PLC")
-        return parser.parse_args()
 
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error(arg + " does not exist")
+    else:
+        return arg
 
 
 if __name__ == "__main__":
-    node_control = NodeControl()
+    parser = argparse.ArgumentParser(description='Start everything for a plc')
+    parser.add_argument(dest="intermediate_yaml",
+                        help="intermediate yaml file", metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("--index", "-i", help="Index of PLC in intermediate yaml", type=int)
+
+    args = parser.parse_args()
+    node_control = NodeControl(Path(args.intermediate_yaml), args.index)
     node_control.main()
