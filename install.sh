82881c6fc9ce5bf5b389a0169782ae7ca25a712c
Simcha Vos <a.s.j.vos@student.tudelft.nl>
2021-05-19 13:21:17 +0200

    Update installation script to have dynamic location

--- install.sh
+++ install.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+cwd=$(pwd)
+
 cd ~
 
 sudo apt update
 
 ## Installing necessary packages
-sudo apt install git
+sudo apt install -y git
+
+sudo apt install -y openvswitch-testcontroller
 
 # Python 2 and pip
-sudo apt install python2
+sudo apt install -y python2
 
-sudo apt install curl
+sudo apt install -y curl
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
 
-# Python 3 and pip
-sudo apt install python3
+# Python 3 and pipd
+sudo apt install -y python3
 
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 # MiniCPS
 git clone https://github.com/afmurillo/minicps.git
@@ -30,12 +34,12 @@ sudo pip install pathlib
 
 sudo pip install pyyaml
 
-sudo pip uninstall cpppo
+sudo pip uninstall -y cpppo
 sudo pip install cpppo==4.0.4
 
-sudo apt install mininet
+sudo apt install -y mininet
 
-cd dhalsim
+cd ${cwd}
 
 sudo python3 -m pip install -e .
 
