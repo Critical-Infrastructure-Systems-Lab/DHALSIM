04320228627fc0d36dbfa5f982c378e844d7de57
Simcha Vos <a.s.j.vos@student.tudelft.nl>
2021-05-20 15:28:18 +0200

    Change logging format, now no longer includes date => H:M:S - INFO @ file.py: Message

--- dhalsim/py3_logger.py
+++ dhalsim/py3_logger.py
@@ -3,5 +3,5 @@ import sys
 
 logging_level = logging.INFO
 logging_format = '%(asctime)s - %(levelname)s @ %(filename)s: %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging_level, format=logging_format)
+logging.basicConfig(stream=sys.stdout, level=logging_level, format=logging_format, datefmt='%H:%M:%S')
 logger = logging.getLogger('py3_logger')
