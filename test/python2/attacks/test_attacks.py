d3f2afd417feb0c95ceaeacb244445af611fc58f
Maarten Weyns <m.b.m.weyns@student.tudelft.nl>
2021-05-20 16:32:04 +0200

    Create test for between attack

--- test/python2/attacks/test_attacks.py
+++ test/python2/attacks/test_attacks.py
@@ -1,7 +1,7 @@
 import sys
 import pytest
 
-from dhalsim.python2.entities.attack import TimeAttack, TriggerBelowAttack, TriggerAboveAttack
+from dhalsim.python2.entities.attack import TimeAttack, TriggerBelowAttack, TriggerAboveAttack, TriggerBetweenAttack
 
 @pytest.fixture
 def time_attack():
@@ -14,6 +14,10 @@ def trigger_attack_above():
 @pytest.fixture
 def trigger_attack_below():
     return TriggerBelowAttack("TestBelowAttack", ["P_RAW1", "P_RAW2"], "closed", "T1", 0.20)
+
+@pytest.fixture
+def trigger_between_attack():
+    return TriggerBetweenAttack("TestBetweenAttack", ["P_RAW1", "P_RAW2"], "closed", "T1", 0.10, 0.16)
 
 # Test that we are running python 2.7
 def test_python_version():
@@ -40,3 +44,11 @@ def test_trigger_below_properties(trigger_attack_below):
     assert trigger_attack_below.command == "closed"
     assert trigger_attack_below.sensor == "T1"
     assert trigger_attack_below.value == 0.20
+
+def test_trigger_between_attack(trigger_between_attack):
+    assert trigger_between_attack.name == "TestBetweenAttack"
+    assert trigger_between_attack.actuators == ["P_RAW1", "P_RAW2"]
+    assert trigger_between_attack.command == "closed"
+    assert trigger_between_attack.sensor == "T1"
+    assert trigger_between_attack.lower_value == 0.10
+    assert trigger_between_attack.upper_value == 0.16
