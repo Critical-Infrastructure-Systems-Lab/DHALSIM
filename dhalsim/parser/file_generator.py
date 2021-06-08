1ab62c61c47a1c947c86bd61d3599ef908a80508
Simcha Vos <a.s.j.vos@student.tudelft.nl>
2021-06-08 19:12:45 +0200

    Add write_batch: this writes a README with details wrt the specific batch that was run.

--- dhalsim/parser/file_generator.py
+++ dhalsim/parser/file_generator.py
@@ -17,19 +17,39 @@ class BatchReadMeGenerator:
         with intermediate_yaml_path.open() as yaml_file:
             self.intermediate_yaml = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
-        # # Create directories in output folder
-        # self.configuration_path = Path(self.intermediate_yaml['output_path']) / 'configuration'
-        # self.input_files_path = self.configuration_path / 'input_files'
-        #
-        # os.makedirs(str(self.input_files_path), exist_ok=True)
+        os.makedirs(str(Path(self.intermediate_yaml['config_path']).parent
+                        / self.intermediate_yaml['output_path']), exist_ok=True)
+        self.readme_path = str(Path(self.intermediate_yaml['config_path']).parent
+                          / self.intermediate_yaml['output_path'] / 'readme_batch.md')
 
     def write_batch(self, start_time, end_time, wn, master_time):
-        readme.write("\n\nRan for {x} out of {y} iterations with hydraulic timestep {step}."
-                     .format(x=str(master_time),
-                             y=str(self.intermediate_yaml['iterations']),
-                             step=str(wn.options.time.hydraulic_timestep)))
+        """
+        Creates a small readme for each batch.
+        :param start_time: is the start time of batch
+        :param end_time: is the end time of batch
+        :param wn: is WNTR instance
+        :param master_time: is current iteration
+        """
+        with open(self.readme_path, 'a') as readme:
+            readme.write("# Auto-generated README of {file} for batch {no}"
+                         .format(file=os.path.basename(str(self.intermediate_yaml['inp_file']))[:-4],
+                                 no=self.intermediate_yaml['batch_index']))
+            readme.write("\n\nThis is batch {x} out of {y} batches."
+                         .format(x=self.intermediate_yaml['batch_index'],
+                                 y=self.intermediate_yaml['batch_iterations']))
+            readme.write("\n\n## Information about this batch")
+            readme.write("\n\nRan for {x} out of {y} iterations with hydraulic timestep {step}."
+                         .format(x=str(master_time),
+                                 y=str(self.intermediate_yaml['iterations']),
+                                 step=str(wn.options.time.hydraulic_timestep)))
+            readme.write("\n\nStarted at {start} and finished at {end}."
+                         .format(start=str(start_time.strftime("%Y-%m-%d %H:%M:%S")),
+                                 end=str(end_time.strftime("%Y-%m-%d %H:%M:%S"))))
+            readme.write("\n\nThe duration of this batch was {time}."
+                         .format(time=str(end_time - start_time)))
+            readme.write("\n\nFor more information with regard to this experiment, consult "
+                         "```readme_experiment.md``` in the root of the output folder.")
 
-        return None
         # batch x / y
         # initial values van deze batch
 
@@ -45,6 +65,7 @@ class InputFilesCopier:
 
     def copy_input_files(self):
         """Copies all input files, mandatory and optional ones included."""
+        os.makedirs(str(self.configuration_folder), exist_ok=True)
 
         # Copy mandatory files.
         with open(str(self.configuration_folder / 'config.yaml'), 'w') as config_file:
