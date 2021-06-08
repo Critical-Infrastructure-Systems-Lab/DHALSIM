ca28090ab3992492b25fd21a220e9262a1b68d26
Simcha Vos <a.s.j.vos@student.tudelft.nl>
2021-06-08 16:07:22 +0200

    Add docstring, add correct output folder print, minor polishing

--- dhalsim/parser/readme_generator.py
+++ dhalsim/parser/readme_generator.py
@@ -43,12 +43,18 @@ class ReadMeGenerator:
 
     def write_md(self, start_time: datetime, end_time: datetime,
                  wn: wntr.network.WaterNetworkModel, master_time: int):
+        """
+        Writes a readme about the current experiment.
+        :param start_time: starting time of experiment
+        :param end_time: ending time of experiment
+        :param wn: WNTR instance
+        :param master_time: current iteration
+        """
         readme_path = str(self.configuration_path / 'readme_experiment.md')
         open(readme_path, 'w+')
 
-        # causes errors in batch mode.. could be solved by having original output path in intermediate yaml
-        #plcs = open(str(self.input_files_path / 'plcs.md'))
-        #plcs.write(self.intermediate_yaml['plcs'])
+        # with open(str(self.configuration_path / 'config2.yaml'), 'w') as file:
+        #     yaml.dump(self.intermediate_yaml, file)
 
         with open(readme_path, 'a') as readme:
             readme.write("# Automatically generated README of "
@@ -56,11 +62,16 @@ class ReadMeGenerator:
 
             # Input files
             readme.write("\n\n## Input files")
-            # TODO: Copy files
-            readme.write("\n\nInput files have been copied to {output}. In case ```attacks_path``` "
-                         "or ```batch_simulations``` was used, these files will be copied to the output "
-                         "folder as well."
-                         .format(output=self.intermediate_yaml['output_path']))
+            input_string = "\n\nInput files have been copied to {output}. In case" \
+                           " ```attacks_path``` or ```batch_simulations``` was used, these files" \
+                           " will be copied to the output folder as well."
+
+            if 'batch_simulations' in self.intermediate_yaml:
+                readme.write(input_string
+                             .format(output=str(Path(self.intermediate_yaml['output_path'])
+                                                .parent)))
+            else:
+                readme.write(input_string.format(output=self.intermediate_yaml['output_path']))
 
             # Configuration parameters
             readme.write("\n\n## Configuration parameters")
@@ -72,14 +83,16 @@ class ReadMeGenerator:
             readme.write(self.get_optional('attacks_path'))
             readme.write(self.get_optional('batch_simulations'))
             readme.write("\n\nAll Mininet links can be found in mininet_links.md.")
+
             # About this experiment
             readme.write("\n\n## About this experiment")
             readme.write("\n\nRan with DHALSIM v{version}."
                          .format(version=pkg_resources.require('dhalsim')[0].version))
-            readme.write("\n\nStarted at {time}."
-                         .format(time=str(start_time.strftime("%Y-%m-%d %H:%M:%S"))))
-            readme.write("\n\nFinished at {time}."
-                         .format(time=str(end_time.strftime("%Y-%m-%d %H:%M:%S"))))
+            readme.write("\n\nStarted at {start} and finished at {end}"
+                         .format(start=str(start_time.strftime("%Y-%m-%d %H:%M:%S")),
+                                 end=str(end_time.strftime("%Y-%m-%d %H:%M:%S"))))
+            readme.write("\n\nThe duration of this simulation was {time}."
+                         .format(time=str(end_time - start_time)))
             readme.write("\n\nRan for {x} out of {y} iterations with hydraulic timestep {step}."
                          .format(x=str(master_time),
                                  y=str(self.intermediate_yaml['iterations']),
