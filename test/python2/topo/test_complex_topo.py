90bc111f1f96aae9e21a9b5aafb96290bba40c32
Robert van Dijk <r.vandijk-10@student.tudelft.nl>
2021-06-10 12:06:36 +0200

    Add test for macs in complex topo

--- test/python2/topo/test_complex_topo.py
+++ test/python2/topo/test_complex_topo.py
@@ -30,7 +30,7 @@ def topo(unmodified_yaml):
 
 @pytest.fixture
 def net(topo):
-    net = Mininet(topo=topo, autoSetMacs=True, link=TCLink)
+    net = Mininet(topo=topo, autoSetMacs=False, link=TCLink)
     net.start()
     topo.setup_network(net)
     time.sleep(0.2)
@@ -40,8 +40,8 @@ def net(topo):
 
 @pytest.mark.integrationtest
 @pytest.mark.parametrize("host1, host2",
-                         [("r0", "r1"), ("r0", "r2"), ("r0", "r3"), ("r1", "PLC1"), ("r2", "PLC2"),
-                          ("r3", "scada")])
+                         [("r0", "r1"), ("r0", "r2"), ("r0", "r3"), ("r2", "PLC1"), ("r3", "PLC2"),
+                          ("r1", "scada")])
 @pytest.mark.flaky(max_runs=3)
 def test_ping(net, host1, host2):
     assert net.ping(hosts=[net.get(host1), net.get(host2)]) == 0.0
@@ -50,10 +50,32 @@ def test_ping(net, host1, host2):
 @pytest.mark.integrationtest
 @pytest.mark.parametrize("host1, host2",
                          [("r0", "r1"), ("r0", "r2"), ("r0", "r3"), ("r1", "s1"), ("r2", "s2"),
-                          ("r3", "s3"), ("s1", "PLC1"), ("s2", "PLC2"),
-                          ("s3", "scada")])
+                          ("r3", "s3"), ("s2", "PLC1"), ("s3", "PLC2"),
+                          ("s1", "scada")])
 def test_links(net, host1, host2):
+    print(net.linksBetween(net.get(host1), net.get(host2)))
     assert net.linksBetween(net.get(host1), net.get(host2)) != []
+
+
+@pytest.mark.integrationtest
+@pytest.mark.parametrize('host1, host2, mac1, mac2',
+                         [('r0', 'r1', 'aa:bb:cc:dd:00:', 'aa:bb:cc:dd:04:'),
+                          ('r0', 'r2', 'aa:bb:cc:dd:00:', 'aa:bb:cc:dd:04:'),
+                          ('r0', 'r3', 'aa:bb:cc:dd:00:', 'aa:bb:cc:dd:04:'),
+                          ('r1', 's1', 'aa:bb:cc:dd:03:', ''),
+                          ('r2', 's2', 'aa:bb:cc:dd:03:', ''),
+                          ('r3', 's3', 'aa:bb:cc:dd:03:', ''),
+                          ('s2', 'PLC1', '', 'aa:bb:cc:dd:02:'),
+                          ('s3', 'PLC2', '', 'aa:bb:cc:dd:02:'),
+                          ('s1', 'scada', '', 'aa:bb:cc:dd:01:')])
+def test_mac_prefix(net, host1, host2, mac1, mac2):
+    links = net.linksBetween(net.get(host1), net.get(host2))
+    assert links != []
+    link = links[0]
+    full_mac_1 = link.intf1.MAC().lower()
+    full_mac_2 = link.intf2.MAC().lower()
+    assert (full_mac_1.startswith(mac1.lower()) and full_mac_2.startswith(mac2.lower())) or (
+            full_mac_1.startswith(mac2.lower()) and full_mac_2.startswith(mac1.lower()))
 
 
 @pytest.mark.integrationtest
@@ -63,9 +85,9 @@ def test_number_of_links(net):
 
 @pytest.mark.integrationtest
 @pytest.mark.parametrize("server, client, server_ip",
-                         [("PLC1", "r0", "10.0.1.1"), ("PLC2", "r0", "10.0.2.1"),
-                          ("PLC1", "PLC2", "10.0.1.1"), ("PLC2", "PLC1", "10.0.2.1"),
-                          ("PLC1", "scada", "10.0.1.1"), ("PLC2", "scada", "10.0.2.1")])
+                         [("PLC1", "r0", "10.0.2.1"), ("PLC2", "r0", "10.0.3.1"),
+                          ("PLC1", "PLC2", "10.0.2.1"), ("PLC2", "PLC1", "10.0.3.1"),
+                          ("PLC1", "scada", "10.0.2.1"), ("PLC2", "scada", "10.0.3.1")])
 @pytest.mark.flaky(max_runs=3)
 def test_reachability(net, server, client, server_ip):
     net.get(server).cmd("echo 'test' | nc -q1 -l 44818 &")
