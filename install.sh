7bff1fe1bd4427ceb5c147e8018f9cedfa48d0b9
Simcha Vos <a.s.j.vos@student.tudelft.nl>
2021-06-11 14:52:34 +0200

    Revert "Fix install script: Mininet was installing for Python 3 instead of Python 2"
    This reverts commit 94a5710a6c1cfb028cf823bc4dbae6dea1774c99.

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ sudo pip install pyyaml
 cd ~
 git clone https://github.com/mininet/mininet.git || git -C mininet pull
 cd mininet
-sudo PYTHON=python2 ./util/install.sh -fnv
+./util/install.sh -fnv
 
 # Install DHALSIM
 cd ${cwd}
