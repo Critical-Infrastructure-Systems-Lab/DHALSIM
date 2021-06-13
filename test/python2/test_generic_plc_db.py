ef8287929712e810626fd4c8af6820537729b1bd
Robert van Dijk <r.vandijk-10@student.tudelft.nl>
2021-06-13 12:37:33 +0200

    Add tests for sleep before db retry

--- test/python2/test_generic_plc_db.py
+++ test/python2/test_generic_plc_db.py
@@ -5,11 +5,12 @@ from mock import call
 
 from dhalsim.python2.generic_plc import GenericPLC, DatabaseError
 
-
 @pytest.fixture
 def patched_plc(mocker):
+    sleeper = mocker.patch("time.sleep", return_value=None)
     mocker.patch.object(GenericPLC, "__init__", return_value=None)
     mocker.patch.object(GenericPLC, "DB_TRIES", 3)
+    mocker.patch.object(GenericPLC, "DB_SLEEP_TIME", 1.5)
     cur_mock = mocker.Mock()
     conn_mock = mocker.Mock()
     logger_mock = mocker.Mock()
@@ -22,24 +23,28 @@ def patched_plc(mocker):
         'name': 'patched_plc'
     }
 
-    return plc, cur_mock, conn_mock, logger_mock
+    return plc, cur_mock, conn_mock, logger_mock, sleeper
 
 
 def test_get_master_clock_first_try(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.fetchone.return_value = [5]
 
     assert plc.get_master_clock() == 5
 
     cur_mock.execute.assert_called_once_with("SELECT time FROM master_time WHERE id IS 1")
+
     cur_mock.fetchone.assert_called_once()
+
     assert logger_mock.debug.call_count == 0
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_not_called()
+
 
 def test_get_master_clock_fail_once(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), None]
 
@@ -50,13 +55,17 @@ def test_get_master_clock_fail_once(patched_plc):
     cur_mock.execute.assert_has_calls([call("SELECT time FROM master_time WHERE id IS 1"),
                                        call("SELECT time FROM master_time WHERE id IS 1")])
     assert cur_mock.execute.call_count == 2
+
     cur_mock.fetchone.assert_called_once()
+
     assert logger_mock.debug.call_count == 1
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_called_once_with(1.5)
+
 
 def test_get_master_clock_fail_all(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), sqlite3.OperationalError(),
                                     sqlite3.OperationalError(), None]
@@ -68,26 +77,35 @@ def test_get_master_clock_fail_all(patched_plc):
                                        call("SELECT time FROM master_time WHERE id IS 1"),
                                        call("SELECT time FROM master_time WHERE id IS 1")])
     assert cur_mock.execute.call_count == 3
+
     cur_mock.fetchone.assert_not_called()
+
     assert logger_mock.debug.call_count == 3
     assert logger_mock.error.call_count == 1
 
+    sleeper.assert_has_calls([call(1.5), call(1.5), call(1.5)])
+    assert sleeper.call_count == 3
+
 
 def test_get_sync_first_try(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.fetchone.return_value = [1]
 
     assert plc.get_sync() is True
 
     cur_mock.execute.assert_called_once_with("SELECT flag FROM sync WHERE name IS ?", ('patched_plc',))
+
     cur_mock.fetchone.assert_called_once()
+
     assert logger_mock.debug.call_count == 0
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_not_called()
+
 
 def test_get_sync_fail_once(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), None]
 
@@ -98,13 +116,17 @@ def test_get_sync_fail_once(patched_plc):
     cur_mock.execute.assert_has_calls([call("SELECT flag FROM sync WHERE name IS ?", ('patched_plc',)),
                                        call("SELECT flag FROM sync WHERE name IS ?", ('patched_plc',))])
     assert cur_mock.execute.call_count == 2
+
     cur_mock.fetchone.assert_called_once()
+
     assert logger_mock.debug.call_count == 1
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_called_once_with(1.5)
+
 
 def test_get_sync_fail_all(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), sqlite3.OperationalError(),
                                     sqlite3.OperationalError(), None]
@@ -116,24 +138,33 @@ def test_get_sync_fail_all(patched_plc):
                                        call("SELECT flag FROM sync WHERE name IS ?", ('patched_plc',)),
                                        call("SELECT flag FROM sync WHERE name IS ?", ('patched_plc',))])
     assert cur_mock.execute.call_count == 3
+
     cur_mock.fetchone.assert_not_called()
+
     assert logger_mock.debug.call_count == 3
     assert logger_mock.error.call_count == 1
 
+    sleeper.assert_has_calls([call(1.5),call(1.5),call(1.5)])
+    assert sleeper.call_count == 3
+
 
 def test_set_sync_first_try(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     plc.set_sync(True)
 
     cur_mock.execute.assert_called_once_with("UPDATE sync SET flag=? WHERE name IS ?", (1, 'patched_plc',))
+
     conn_mock.commit.assert_called_once()
+
     assert logger_mock.debug.call_count == 0
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_not_called()
+
 
 def test_set_sync_fail_once(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), None]
 
@@ -142,13 +173,17 @@ def test_set_sync_fail_once(patched_plc):
     cur_mock.execute.assert_has_calls([call("UPDATE sync SET flag=? WHERE name IS ?", (1, 'patched_plc',)),
                                        call("UPDATE sync SET flag=? WHERE name IS ?", (1, 'patched_plc',))])
     assert cur_mock.execute.call_count == 2
+
     conn_mock.commit.assert_called_once()
+
     assert logger_mock.debug.call_count == 1
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_called_once_with(1.5)
+
 
 def test_set_sync_fail_all(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), sqlite3.OperationalError(),
                                     sqlite3.OperationalError(), None]
@@ -160,24 +195,33 @@ def test_set_sync_fail_all(patched_plc):
                                        call("UPDATE sync SET flag=? WHERE name IS ?", (1, 'patched_plc',)),
                                        call("UPDATE sync SET flag=? WHERE name IS ?", (1, 'patched_plc',))])
     assert cur_mock.execute.call_count == 3
+
     conn_mock.commit.assert_not_called()
+
     assert logger_mock.debug.call_count == 3
     assert logger_mock.error.call_count == 1
 
+    sleeper.assert_has_calls([call(1.5),call(1.5),call(1.5)])
+    assert sleeper.call_count == 3
+
 
 def test_set_attack_flag_first_try(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     plc.set_attack_flag(True, 'test_attack')
 
     cur_mock.execute.assert_called_once_with("UPDATE attack SET flag=? WHERE name IS ?", (1, 'test_attack',))
+
     conn_mock.commit.assert_called_once()
+
     assert logger_mock.debug.call_count == 0
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_not_called()
+
 
 def test_set_attack_flag_fail_once(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), None]
 
@@ -186,13 +230,17 @@ def test_set_attack_flag_fail_once(patched_plc):
     cur_mock.execute.assert_has_calls([call("UPDATE attack SET flag=? WHERE name IS ?", (1, 'test_attack',)),
                                        call("UPDATE attack SET flag=? WHERE name IS ?", (1, 'test_attack',))])
     assert cur_mock.execute.call_count == 2
+
     conn_mock.commit.assert_called_once()
+
     assert logger_mock.debug.call_count == 1
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_called_once_with(1.5)
+
 
 def test_set_attack_flag_fail_all(patched_plc):
-    plc, cur_mock, conn_mock, logger_mock = patched_plc
+    plc, cur_mock, conn_mock, logger_mock, sleeper = patched_plc
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), sqlite3.OperationalError(),
                                     sqlite3.OperationalError(), None]
@@ -204,6 +252,11 @@ def test_set_attack_flag_fail_all(patched_plc):
                                        call("UPDATE attack SET flag=? WHERE name IS ?", (1, 'test_attack',)),
                                        call("UPDATE attack SET flag=? WHERE name IS ?", (1, 'test_attack',))])
     assert cur_mock.execute.call_count == 3
+
     conn_mock.commit.assert_not_called()
+
     assert logger_mock.debug.call_count == 3
     assert logger_mock.error.call_count == 1
+
+    sleeper.assert_has_calls([call(1.5),call(1.5),call(1.5)])
+    assert sleeper.call_count == 3
--- test/python2/test_generic_scada_db.py
+++ test/python2/test_generic_scada_db.py
@@ -7,8 +7,10 @@ from mock import call
 
 @pytest.fixture
 def patched_scada(mocker):
+    sleeper = mocker.patch("time.sleep", return_value=None)
     mocker.patch.object(GenericScada, "__init__", return_value=None)
     mocker.patch.object(GenericScada, "DB_TRIES", 3)
+    mocker.patch.object(GenericScada, "DB_SLEEP_TIME", 1.5)
     cur_mock = mocker.Mock()
     conn_mock = mocker.Mock()
     logger_mock = mocker.Mock()
@@ -18,24 +20,28 @@ def patched_scada(mocker):
     scada.conn = conn_mock
     scada.logger = logger_mock
 
-    return scada, cur_mock, conn_mock, logger_mock
+    return scada, cur_mock, conn_mock, logger_mock, sleeper
 
 
 def test_get_sync_first_try(patched_scada):
-    scada, cur_mock, conn_mock, logger_mock = patched_scada
+    scada, cur_mock, conn_mock, logger_mock, sleeper = patched_scada
 
     cur_mock.fetchone.return_value = [1]
 
     assert scada.get_sync() is True
 
     cur_mock.execute.assert_called_once_with("SELECT flag FROM sync WHERE name IS 'scada'")
+
     cur_mock.fetchone.assert_called_once()
+
     assert logger_mock.debug.call_count == 0
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_not_called()
+
 
 def test_get_sync_fail_once(patched_scada):
-    scada, cur_mock, conn_mock, logger_mock = patched_scada
+    scada, cur_mock, conn_mock, logger_mock, sleeper = patched_scada
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), None]
 
@@ -46,13 +52,17 @@ def test_get_sync_fail_once(patched_scada):
     cur_mock.execute.assert_has_calls([call("SELECT flag FROM sync WHERE name IS 'scada'"),
                                        call("SELECT flag FROM sync WHERE name IS 'scada'")])
     assert cur_mock.execute.call_count == 2
+
     cur_mock.fetchone.assert_called_once()
+
     assert logger_mock.debug.call_count == 1
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_called_once_with(1.5)
+
 
 def test_get_sync_fail_all(patched_scada):
-    scada, cur_mock, conn_mock, logger_mock = patched_scada
+    scada, cur_mock, conn_mock, logger_mock, sleeper = patched_scada
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), sqlite3.OperationalError(),
                                     sqlite3.OperationalError(), None]
@@ -64,24 +74,33 @@ def test_get_sync_fail_all(patched_scada):
                                        call("SELECT flag FROM sync WHERE name IS 'scada'"),
                                        call("SELECT flag FROM sync WHERE name IS 'scada'")])
     assert cur_mock.execute.call_count == 3
+
     cur_mock.fetchone.assert_not_called()
+
     assert logger_mock.debug.call_count == 3
     assert logger_mock.error.call_count == 1
 
+    sleeper.assert_has_calls([call(1.5), call(1.5), call(1.5)])
+    assert sleeper.call_count == 3
+
 
 def test_set_sync_first_try(patched_scada):
-    scada, cur_mock, conn_mock, logger_mock = patched_scada
+    scada, cur_mock, conn_mock, logger_mock, sleeper = patched_scada
 
     scada.set_sync(True)
 
     cur_mock.execute.assert_called_once_with("UPDATE sync SET flag=? WHERE name IS 'scada'", (1,))
+
     conn_mock.commit.assert_called_once()
+
     assert logger_mock.debug.call_count == 0
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_not_called()
+
 
 def test_set_sync_fail_once(patched_scada):
-    scada, cur_mock, conn_mock, logger_mock = patched_scada
+    scada, cur_mock, conn_mock, logger_mock, sleeper = patched_scada
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), None]
 
@@ -90,13 +109,17 @@ def test_set_sync_fail_once(patched_scada):
     cur_mock.execute.assert_has_calls([call("UPDATE sync SET flag=? WHERE name IS 'scada'", (1,)),
                                        call("UPDATE sync SET flag=? WHERE name IS 'scada'", (1,))])
     assert cur_mock.execute.call_count == 2
+
     conn_mock.commit.assert_called_once()
+
     assert logger_mock.debug.call_count == 1
     assert logger_mock.error.call_count == 0
 
+    sleeper.assert_called_once_with(1.5)
+
 
 def test_set_sync_fail_all(patched_scada):
-    scada, cur_mock, conn_mock, logger_mock = patched_scada
+    scada, cur_mock, conn_mock, logger_mock, sleeper = patched_scada
 
     cur_mock.execute.side_effect = [sqlite3.OperationalError(), sqlite3.OperationalError(),
                                     sqlite3.OperationalError(), None]
@@ -108,6 +131,11 @@ def test_set_sync_fail_all(patched_scada):
                                        call("UPDATE sync SET flag=? WHERE name IS 'scada'", (1,)),
                                        call("UPDATE sync SET flag=? WHERE name IS 'scada'", (1,))])
     assert cur_mock.execute.call_count == 3
+
     conn_mock.commit.assert_not_called()
+
     assert logger_mock.debug.call_count == 3
     assert logger_mock.error.call_count == 1
+
+    sleeper.assert_has_calls([call(1.5), call(1.5), call(1.5)])
+    assert sleeper.call_count == 3
