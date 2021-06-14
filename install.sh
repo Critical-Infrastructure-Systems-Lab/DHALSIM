9a873b4c4614433a5dc1a0069b3bfde6e0868342
Simcha Vos <a.s.j.vos@student.tudelft.nl>
2021-06-14 15:28:23 +0200

    Add test option to install script

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+ARG1=${1:-test}
 cwd=$(pwd)
 version=$(lsb_release -rs )
 
 if [ "$version" != "20.04" ]
 then
   echo "Warning! This installation script has only been tested on Ubuntu 20.04 LTS and will likely not work on other versions."
+fi
+
+if [ "$ARG1" != "test" ]
+then
+  echo "Installing without testing dependencies."
+  sleep 3
+else
+  echo "Installing with testing dependencies."
   sleep 3
 fi
 
@@ -33,6 +42,14 @@ sudo python2 -m pip install .
 sudo pip install pathlib==1.0.*
 sudo pip install pyyaml==5.3.*
 
+## Installing testing dependencies
+if [ "$ARG1" != "test" ]
+then
+  sudo pip2 install netaddr==0.8.*
+  sudo pip2 install flaky==3.7.*
+  sudo pip2 install pytest==4.6.*
+fi
+
 # Mininet from source
 cd ~
 git clone --depth 1 -b 2.3.0 https://github.com/mininet/mininet.git || git -C mininet pull
@@ -41,7 +58,13 @@ sudo PYTHON=python2 ./util/install.sh -fnv
 
 # Install DHALSIM
 cd "${cwd}" || { echo "Failure: Could not find DHALSIM directory"; exit 1; }
-sudo python3 -m pip install -e .
+
+if [ "$ARG1" != "test" ]
+then
+  sudo python3 -m pip install -e .
+else
+  sudo python3 -m pip install -e .[test]
+fi
 sudo service openvswitch-switch start
 
 # Installation complete
