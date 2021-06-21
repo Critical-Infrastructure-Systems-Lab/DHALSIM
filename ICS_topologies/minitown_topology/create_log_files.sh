00dcf7809b43d3dc1d0177a3e7a9c2ac22db78af
Andres <andres.murillo.19@gmail.com>
2021-06-21 16:58:56 +0800

    Fixing new lines and execution flags

--- ICS_topologies/minitown_topology/create_log_files.sh
+++ ICS_topologies/minitown_topology/create_log_files.sh
@@ -1,1 +1,8 @@
-#!/bin/bashfiles=(plc1 plc2 scada physical)rm -rf output/*for file in "${files[@]}"  do      touch output/$file".log"done
+#!/bin/bash
+files=(plc1 plc2 scada physical)
+rm -rf output/*
+for file in "${files[@]}"
+ do
+ touch output/$file".log"
+done
+
