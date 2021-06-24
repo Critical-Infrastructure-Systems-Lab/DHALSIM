19bd971dc651bcf588370ae05a235ef643955346
Maarten Weyns <maarten.weyns@mawey.be>
2021-06-24 14:32:07 +0200

    Update project URL in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="dhalsim",
     version="1.1.0",
-    url="https://gitlab.ewi.tudelft.nl/cse2000-software-project/2020-2021-q4/cluster-06/water-infrastructure/dhalsim",
+    url="https://github.com/afmurillo/DHALSIM",
     project_urls={
         "Bug Tracker": "https://gitlab.ewi.tudelft.nl/cse2000-software-project/2020-2021-q4/cluster-06/water-infrastructure/dhalsim/-/issues",
     },
