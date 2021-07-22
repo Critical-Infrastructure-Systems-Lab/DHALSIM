f5b6fa8803642058c354108e46ddb75926bed658
Simcha Vos <simchavos.bedrijven@gmail.com>
2021-07-22 19:49:16 +0200

    Add an optional parameter 'noise_scale' to the config schema

--- dhalsim/parser/config_parser.py
+++ dhalsim/parser/config_parser.py
@@ -291,6 +291,9 @@ class SchemaParser:
                 str,
                 Use(str.lower),
                 Or('pdd', 'dd'), error="'simulator' should be one of the following: 'pdd' or 'dd'."),
+            Optional('noise_scale', default=0): And(
+                float,
+                Schema(lambda i: i >= 0, error="'noise_scale' must be positive.")),
             Optional('attacks'): {
                 Optional('device_attacks'): [SchemaParser.device_attacks],
                 Optional('network_attacks'): [SchemaParser.network_attacks],
