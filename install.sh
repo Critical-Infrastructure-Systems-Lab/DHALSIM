99aa1a64fbb51fdb90f67abf32e11f105196deff
Afmurillo <andres_murillo@sutd.edu.sg>
2021-08-02 22:35:08 +0800

    Fixing the install script

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ sudo python2 -m pip install .
 # Installing other DHALSIM dependencies
 sudo pip install pathlib==1.0.*
 sudo pip install pyyaml==5.3.*
+sudo pip install numpy==1.16.*
 
 # Mininet from source
 cd ~
