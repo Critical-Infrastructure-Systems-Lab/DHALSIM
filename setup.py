a0be35275a6f2b2ae1a7f4011822fec49b81807f
Andrés F. Murillo <afmurillo@users.noreply.github.com>
2022-02-01 20:08:22 +0800

    Update setup.py
    PyYAML was also generating conflict

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     license='MIT',
     packages=['dhalsim'],
     install_requires=[
-       'PyYAML',
+       'PyYAML==5.4.1',
         'pyyaml-include',
         'antlr4-python3-runtime==4.7.2',
         'progressbar2',
